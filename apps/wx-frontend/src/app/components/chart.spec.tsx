fb00f56a533b40c3f30a91ebd469572dc7740c20
sanderblue <william.a.blue@gmail.com>
2020-01-20 15:55:47 -0800

    fix: commit lowercase chart filename

---
+++ apps/wx-frontend/src/app/components/chart.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Chart from './chart';
+
+describe(' Chart', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Chart />);
+    expect(baseElement).toBeTruthy();
+  });
+});

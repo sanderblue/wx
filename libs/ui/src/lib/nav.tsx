f7b4fc7af33089d4b79bdd6d080b66c7397b7af3
sanderblue <william.a.blue@gmail.com>
2020-06-07 16:44:32 -0700

    feat: better responsive styles for nav search form

--- libs/ui/src/lib/nav.tsx
+++ libs/ui/src/lib/nav.tsx
@@ -57,14 +57,17 @@ export const Nav = (props: NavProps) => {
 
   return (
     <nav className="bg-gray-900 p-3 text-white">
-      <form className="flex justify-between" onSubmit={onSubmitForm}>
-        <div className="mr-4 w-108 relative">
+      <form
+        className="flex justify-between lg:justify-center"
+        onSubmit={onSubmitForm}
+      >
+        <div className="mr-4 lg:mr-8 w-108 lg:w-112 relative">
           <label className="block text-gray-500 text-xs font-bold">
             Search
           </label>
           <Autocomplete onSelectResult={onSelectSearchResult}></Autocomplete>
         </div>
-        <div className="mr-2">
+        <div className="mr-2 lg:mr-8">
           <label className="block text-gray-500 text-xs font-bold">
             Start Date
           </label>
@@ -75,7 +78,7 @@ export const Nav = (props: NavProps) => {
             className="w-full bg-gray-800 text-sm text-white transition border border-transparent focus:outline-none focus:border-gray-700 rounded py-1 px-2 appearance-none leading-normal"
           />
         </div>
-        <div className="mr-2">
+        <div className="mr-2 lg:mr-8">
           <label className="block text-gray-500 text-xs font-bold">
             End Date
           </label>
@@ -83,11 +86,11 @@ export const Nav = (props: NavProps) => {
             defaultValue={defaultEndDate}
             onChange={onChangeEndDate}
             placeholder="YYYY-MM-DD"
-            className="w-full bg-gray-800 text-sm text-white transition border border-transparent focus:outline-none focus:border-gray-700 rounded py-1 px-2 appearance-none leading-normal"
+            className="w-full py-1 px-2 bg-gray-800 text-sm text-white transition border border-transparent focus:outline-none focus:border-gray-700 rounded appearance-none leading-normal"
           />
         </div>
         <div className="flex mr-2 last:mr-0">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1 rounded self-end">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1 lg:px-8 rounded self-end">
             Go
           </button>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
         '84': '21rem',
         '96': '24rem',
         '108': '30rem',
+        '112': '32rem',
+        '120': '36rem',
       },
       minHeight: {
         '0': '0',

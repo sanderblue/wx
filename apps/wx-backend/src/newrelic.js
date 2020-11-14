c95592e8077f23357eb5f9b0ab834d852f9d271b
sanderblue <william.a.blue@gmail.com>
2020-11-14 16:06:47 -0600

    chore(wx-backend): reduce newrelic node agent log level to error

--- apps/wx-backend/src/newrelic.js
+++ apps/wx-backend/src/newrelic.js
@@ -26,7 +26,7 @@ exports.config = {
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: 'trace',
+    level: 'error',
   },
   /**
    * When true, all request headers except for those listed in attributes.exclude
